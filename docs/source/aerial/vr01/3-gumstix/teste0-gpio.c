a40ab1e7fe3cb890bb06caa7e0a31eefb68611cd
LucasCampos98 <lukecampos29@gmail.com>
2020-07-01 14:10:12 -0300

    teste0

---
+++ docs/source/aerial/vr01/3-gumstix/teste0-gpio.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+int main()
+{
+    int i;
+    int arq = open("/sys/class/gpio/export", O_WRONLY);
+    write(arq, "10", 2);
+    close(arq);
+
+    arq = open("/sys/class/gpio/gpio10/direction", O_WRONLY);
+    write(arq, "out", 3);
+    close(arq);
+
+    arq = open("/sys/class/gpio/gpio10/value", O_RDWR);
+    
+    system("cls");
+
+    do
+    {
+
+        printf("0 ->bit 0 \n1 ->bit 1\n2 ->SAIR\n");
+        scanf("%i", &i);
+
+        switch (i)
+        {
+        case 0:
+        {
+            write(arq, "0", 1);
+            printf("Saida = 0\n\n");
+        }
+
+        case 1:
+        {
+            write(arq, "1", 1);
+            printf("Saida = 1\n\n");
+        }
+        case 2:
+            break;
+        }
+        while (i != 2);
+
+        close(arq);
+
+        return 0;
+    }
